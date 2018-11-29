31e756f3d8650c11bf6eb66ee3dc6e026aca07ee
Imam Fauzan <imamfzn@gmail.com>
2018-11-29 16:33:29 +0700

    override equal

--- lib/bukaresep/recipe.rb
+++ lib/bukaresep/recipe.rb
@@ -15,7 +15,17 @@ module Bukaresep
 
 		def to_json
 			{ id: @id, name: @name, description: @description, ingredients: @ingredients, instructions: @instructions }.to_json
-		end 
+		end
+
+		def ==(other)
+			raise TypeError, 'Other mas be a Recipe' unless other.instance_of? Recipe
+
+			@id == other.id && 
+				@name == other.name &&
+				@description == other.description &&
+				@ingredients == other.ingredients &&
+				@instructions == other.instructions
+		end
 
 		def valid?
 			return false if (@name.nil? || @name == "" || @description.nil? || @description == "" || 
